6788cfaf40be6406215e0f78420abfdd457790fd
greenman999 <mail@greenman999.de>
2025-05-31 21:08:10 +0200

    Update keymap layout to swap to colemak dh for hedgehoc keyboard

--- keyboards/hedgehoc/keymaps/default/keymap.c
+++ keyboards/hedgehoc/keymaps/default/keymap.c
@@ -12,11 +12,11 @@ void keyboard_pre_init_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
-      * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
+      * │ Q │ W │ F │ P │ B │       │ J │ L │ U │ Y │ ; │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * │ A │ R │ S │ T │ G │       │ M │ N │ E │ I │ O │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
+      * │ Z │ X │ C │ D │ V │       │ K │ H │ , │ . │ / │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
       *           ┌───┐                   ┌───┐
       *           │GUI├───┐           ┌───┤Alt│
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┘   └───┘
       */
     [0] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                   KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
+        KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                               KC_J,    KC_L,          KC_U,           KC_Y,           KC_SCLN,
+        KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                               KC_M,    KC_N,          KC_E,           KC_I,           KC_O,
+        KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,                               KC_K,    KC_H,          KC_COMM,        KC_DOT,         KC_SLSH,
+                                                                    KC_ESC, KC_SPC, MO(1),              MO(2),  KC_ENT,  KC_BSPC
     ),
     [1] = LAYOUT_split_3x5_3(
       /*
