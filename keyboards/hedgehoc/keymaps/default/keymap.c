bd0861125866e570aa00ba311f912d357d44f491
greenman999 <mail@greenman999.de>
2025-08-06 01:58:05 +0200

    Update default keymap layout for hedgehoc keyboard with layer definitions and key modifications

--- keyboards/hedgehoc/keymaps/default/keymap.c
+++ keyboards/hedgehoc/keymaps/default/keymap.c
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *               └───┤   │   │   ├───┘
       *                   └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x5_3(
+    [_DEFAULT] = LAYOUT_split_3x5_3(
         KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                               KC_J,    KC_L,          KC_U,           KC_Y,           KC_SCLN,
-        KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                               KC_M,    KC_N,          KC_E,           KC_I,           KC_O,
+        LGUI_T(KC_A),   LALT_T(KC_R),   LCTL_T(KC_S),   LSFT_T(KC_T),   KC_G,                               KC_M,    LSFT_T(KC_N),  LCTL_T(KC_E),   LALT_T(KC_I),   LGUI_T(KC_O),
         KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,                               KC_K,    KC_H,          KC_COMM,        KC_DOT,         KC_SLSH,
-                                                                    KC_ESC, KC_SPC, MO(1),              MO(2),  KC_ENT,  KC_BSPC
+                                                                    KC_ESC, KC_SPC, MO(_NUMBERS),              MO(_SYMBOLS),  KC_ENT,  KC_BSPC
     ),
     [_NUMBERS] = LAYOUT_split_3x5_3(
       /*
@@ -75,10 +75,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        *               └───┤Spc│   │Ent├───┘
        *                   └───┘   └───┘
        */
-      KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,                               KC_5,    KC_6,    KC_7,    KC_8,    KC_9,
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX,
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                     KC_LGUI, _______, KC_SPC,           KC_ENT, MO(3), KC_RALT
+      KC_TAB,   XXXXXXX,    KC_7,   KC_8,   KC_9,                           KC_DOWN,    KC_RIGHT,   XXXXXXX,    XXXXXXX,    XXXXXXX,
+      KC_LCTL,  XXXXXXX,    KC_4,   KC_5,   KC_6,                           XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+      KC_LSFT,  KC_0,       KC_1,   KC_2,   KC_3,                           KC_UP,      KC_LEFT,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+                                        KC_LGUI, _______, KC_SPC,           KC_ENT, MO(_RGB), KC_RALT
   ),
   [_SYMBOLS] = LAYOUT_split_3x5_3(
       /*
